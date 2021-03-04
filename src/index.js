9afc8826efc1da05a878cdc01d60c3721326f006
alexpodelinskii <nikitik00787@gmail.com>
2021-03-05 01:40:01 +0300

    feat: add function
    Signed-off-by: alexpodelinskii <nikitik00787@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    array.sort((a, b) => a - b);
+    return array[0];
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    array.sort((a, b) => b - a);
+    return array[0];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    let summ = array.reduce((sum, cur) => sum + cur);
+    return summ / array.length;
+
 }
